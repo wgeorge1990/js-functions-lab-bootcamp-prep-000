9d5643378c096fbb5221825c8f6c3fe54ddb3768
williameverettgeorge@icloud.com <williameverettgeorge@icloud.com>
2018-09-01 05:02:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 //write your code below!
-
 function happyHolidays() {
   return "Happy holidays!"
 }
-
-function happyHolidaysTo() {
-  console.log(`Happy holidays, ${Name}!`)
+ function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
 }
-
-function happyHolidaysTo(){
-  console.log(`Happy ${holiday}, ${name}!`
+ function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(){
-  console.log(`It's ${days} days until ${holiday}!`)
+ function holidayCountdown(holiday, days){
+  return `It\'s ${days} days until ${holiday}!`
 }
