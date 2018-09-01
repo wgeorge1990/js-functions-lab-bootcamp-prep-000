c5ee5ae9a0c3ebf2d0c926c7bf774ec200b3c7c9
williameverettgeorge@icloud.com <williameverettgeorge@icloud.com>
2018-09-01 04:58:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// write your code below!
+function happyHolidays() {
+return "Happy holidays!"
+}
+
+function happyHolidaysTo() {
+  console.log(`Happy holidays, ${Name}!`)
+}
+
+function happyHolidaysTo(){
+  console.log(`Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(){
+  console.log(`It's ${days} days until ${holiday}!`)
+}
